b70e17cbb020a6e05603473251b9bf249ec2cd38
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-01-06 00:49:01 +0100

    chore:rename images & added the dest top component

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,10 +1,11 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Grid } from "@chakra-ui/react";
 import React from "react";
 import CourseTab from "../../Components/Coursetab/CourseTab";
 import Header from "../../Components/Header";
 import images from "../../Components/images.json";
 import { Flex } from "@chakra-ui/react";
 const webdevelopment = images[0].webdevelopment;
+const desktoppublishing = images[0].desktoppublishing;
 
 function index() {
   return (
@@ -23,13 +24,56 @@ function index() {
         </Box>
       </Flex>
       {/* end of the courses text */}
-      <CourseTab
-        to="courses/webdevelopment"
-        courseText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos dignissimos tempora odit consectetur, libero natus, quam magni distinctio animi quasi odio nemo quaerat veritatis velit voluptatem ullam maiores. Minima."
-        courseTitle="web development"
-        courseImage={webdevelopment}
-        price="50000"
-      />
+      <Grid
+        templateColumns="repeat(3, 1fr)"
+        mr={10}
+        ml={10}
+        rowGap={7}
+        placeItems="center"
+      >
+        <CourseTab
+          to="courses/webdevelopment"
+          courseText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos dignissimos tempora odit consectetur, libero natus, quam magni distinctio animi quasi odio nemo quaerat veritatis velit voluptatem ullam maiores. Minima."
+          courseTitle="Web Development"
+          courseImage={webdevelopment}
+          price="50000"
+        />
+        <CourseTab
+          to="courses/webdevelopment"
+          courseText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos dignissimos tempora odit consectetur, libero natus, quam magni distinctio animi quasi odio nemo quaerat veritatis velit voluptatem ullam maiores. Minima."
+          courseTitle="Computer Programming"
+          courseImage={webdevelopment}
+          price="50000"
+        />
+        <CourseTab
+          to="courses/webdevelopment"
+          courseText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos dignissimos tempora odit consectetur, libero natus, quam magni distinctio animi quasi odio nemo quaerat veritatis velit voluptatem ullam maiores. Minima."
+          courseTitle="Desktop Publishing"
+          courseImage={desktoppublishing}
+          price="50000"
+        />
+        <CourseTab
+          to="courses/webdevelopment"
+          courseText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos dignissimos tempora odit consectetur, libero natus, quam magni distinctio animi quasi odio nemo quaerat veritatis velit voluptatem ullam maiores. Minima."
+          courseTitle="Office Application"
+          courseImage={webdevelopment}
+          price="50000"
+        />
+        <CourseTab
+          to="courses/webdevelopment"
+          courseText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos dignissimos tempora odit consectetur, libero natus, quam magni distinctio animi quasi odio nemo quaerat veritatis velit voluptatem ullam maiores. Minima."
+          courseTitle="Business Accounting"
+          courseImage={webdevelopment}
+          price="50000"
+        />
+        <CourseTab
+          to="courses/webdevelopment"
+          courseText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos dignissimos tempora odit consectetur, libero natus, quam magni distinctio animi quasi odio nemo quaerat veritatis velit voluptatem ullam maiores. Minima."
+          courseTitle="Certified Graphic Pro"
+          courseImage={webdevelopment}
+          price="50000"
+        />
+      </Grid>
     </Box>
   );
 }
