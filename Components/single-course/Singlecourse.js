822a79933dd9503ff63ae15c61eb39d664e1cd7e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-01-17 13:35:30 +0100

    bug:fixed bug on styles

--- Components/single-course/Singlecourse.js
+++ Components/single-course/Singlecourse.js
@@ -161,25 +161,26 @@ const Singlecourse = (props) => {
           </div>
           {/* the right side of the single page of the courses */}
           {/* <div> */}
-          <div className="single-course-content-two">
-            <div className="header">Get this Course</div>
-            <div className="content">
-              Augue neque gravida in fermentum et sollicitudin ac orci. Velit
-              egestas dui id
+          <Box>
+            <div className="single-course-content-two">
+              <div className="course-header">Get this Course</div>
+              <div className="content">
+                Augue neque gravida in fermentum et sollicitudin ac orci. Velit
+                egestas dui id
+              </div>
+              <div>
+                <select id="select-courses" className="select-course">
+                  <option selected>Course</option>
+                </select>
+              </div>
+              <div className="course-amount">
+                <span className="amount"> $ 699.00 USD</span>
+                <span className="discount">$ 1,200.00 USD</span>
+              </div>
+              <button className="course-cart-button">Add to cart</button>
             </div>
-            <div>
-              <select id="select-courses" className="select-course">
-                <option selected>Course</option>
-              </select>
-            </div>
-            <div className="course-amount">
-              <span className="amount"> $ 699.00 USD</span>
-              <span className="discount">$ 1,200.00 USD</span>
-            </div>
-            <button className="course-cart-button">Add to cart</button>
-          </div>
-          {/* </div> */}
-          <p>lorem5</p>
+            <p>lorem5</p>
+          </Box>
         </div>
         {/* the "what is include part of the single course page " */}
         <Box boxShadow="md" rounded="md" bg="white" p="0rem 2rem 2rem 2rem">
