b3be4c8a17b33c6205dc4d8c8a78b8321a2b7dd3
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-01-18 11:16:36 +0100

    chore: finished desktop page

--- pages/courses/desktoppublishing.js
+++ pages/courses/desktoppublishing.js
@@ -7,7 +7,52 @@ function desktoppublishing() {
   return (
     <Box>
       <Header />
-      <Singlecourse />
+      <Singlecourse
+        courseTitle="Ultimate Desktop Publishing Course"
+        courseDetails="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nuncsem at tortor egestas posuere sed"
+        aboutCourse="What’s this course about?"
+        aboutCourseText=" Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Cupiditate adipisci fuga nam veniam
+        perferendis,
+         quidem recusandae cum voluptatem
+        debitis. Enim omnis illum laboriosam dolorem blanditiis neque
+        repellat voluptas! Enim, autem?
+        Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Autem voluptate et maiores quasi amet iste
+        sunt eligendi repudiandaequisquam
+        consequatur!"
+        listOne=" Lorem ipsum dolor sit amet consectetur
+        adipisicing elit."
+        listTwo=" Lorem ipsum dolor sit amet consectetur
+        adipisicing elit."
+        listThree=" Lorem ipsum dolor sit amet consectetur
+        adipisicing elit."
+        aboutCourseTextTwo="Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Optio atque nulla veritatis dolorem esse ad
+        quibusdam ipsam doloribus, dolor vero
+        voluptates alias porro mollitia architecto ex magni magnam?
+        Aut, consequatur."
+        whatToLearn="What you’ll learn"
+        whatToLearnText="  Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Cupiditate adipisci fuga nam veniam
+        perferendis,
+         quidem recusandae cum voluptatem
+        debitis. Enim omnis illum laboriosam dolorem blanditiis neque
+        repellat voluptas! Enim, autem?"
+        whatToLearnListOne="Lorem ipsum dolor sit amet consectetur
+        adipisicing elit."
+        whatToLearnListTwo="Lorem ipsum dolor sit amet consectetur
+        adipisicing elit."
+        whatToLearnListThree="Lorem ipsum dolor sit amet consectetur
+        adipisicing elit."
+        whatToLearnListFour="Lorem ipsum dolor sit amet consectetur
+        adipisicing elit."
+        whatToLearnListFive="Lorem ipsum dolor sit amet consectetur
+        adipisicing elit."
+        whatToLearnListSix="Lorem ipsum dolor sit amet consectetur
+        adipisicing elit."
+        joinClass="Join the Class"
+      />
     </Box>
   );
 }
