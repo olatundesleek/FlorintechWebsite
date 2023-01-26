4e81e5164af341973195a5114ccc1f9134b21e9a
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-01-26 10:48:47 +0100

    chore:contact input

--- Components/Contact/Contact.js
+++ Components/Contact/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <>
       <Box className="faq-container">
-        <h3>Contact</h3>
+        <h3>CONTACT</h3>
         <h1>Get in touch.</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
@@ -16,7 +16,20 @@ const Contact = () => {
         </p>
       </Box>
       <Box>
-        <Box></Box>
+        <Box>
+          <form action="" method="post">
+            <div>
+              <Box>
+                <label>Name</label>
+              </Box>
+              <input
+                type="text"
+                class="contact-input"
+                placeholder="John Carter"
+              />
+            </div>
+          </form>
+        </Box>
         <Box></Box>
       </Box>
     </>
