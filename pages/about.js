ddc0e7cc0b200ddc00c9602733227d957985c223
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-03 13:25:26 +0100

    chore:added the about title and texts

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import { Box } from "@chakra-ui/react";
 import About from "../Components/About/About";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
@@ -16,6 +17,17 @@ const about = () => {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Header />
+      <Box className="faq-container">
+        <h3>ABOUT US</h3>
+        <h1>IT solution at it's best.The Future</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+          aspernatur voluptatem commodi quis. Dolorem nulla officiis quae ullam?
+          Mollitia cumque odio, laborum in molestias itaque sed deserunt illum
+          accusantium blanditiis dolore adipisci perferendis atque quia eum
+          reiciendis hic quidem perspiciatis.
+        </p>
+      </Box>
       <About />
       <Footer />
     </div>
