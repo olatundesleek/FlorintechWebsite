2d5fe2fe798092acb4f11c768f1dfc03a45ab4f8
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-03 13:14:49 +0100

    chore:reposition of the about page

--- Components/About/About.js
+++ Components/About/About.js
@@ -1,22 +1,12 @@
 import { Box } from "@chakra-ui/react";
-import Head from "next/head";
+
 import React from "react";
-import Header from "../Header";
+// import Header from "../Header";
 
 const About = () => {
   return (
     <>
-      <div className="">
-        <Head>
-          <title>About | Florintech Computer College</title>
-          <meta
-            name="courses"
-            content="you can select from our wide range of courses"
-          />
-          <link rel="icon" href="/favicon.png" />
-        </Head>
-        <Header />
-      </div>
+        <h1>About</h1>
     </>
   );
 };
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Head from "next/head";
 import About from "../Components/About/About";
+import Header from "../Components/Header";
 
 const about = () => {
   return (
     <div>
+      <Head>
+        <title>About | Florintech Computer College</title>
+        <meta
+          name="courses"
+          content="you can select from our wide range of courses"
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Header />
       <About />
     </div>
   );
