93c9ececf4e58a0892314e60b7831f4cc6221ea4
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-03 16:54:52 +0100

    chore:added the client stories

--- Components/About/About.js
+++ Components/About/About.js
@@ -1,6 +1,7 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
 import aboutUs from "../../public/images/2.jpeg";
+import aboutUs2 from "../../public/images/businesselectronicaccounting.jpg";
 import React from "react";
 
 const About = () => {
@@ -32,7 +33,7 @@ const About = () => {
             </Box>
           </Flex>
         </Box>
-        {/* end of the about page */}
+        {/* end of the about page  and begin the award */}
         <Flex justifyContent="center">
           <Box className="award__class">
             <h2>2 345</h2>
@@ -51,6 +52,36 @@ const About = () => {
             <p>Dedicated Volunteers</p>
           </Box>
         </Flex>
+        {/* begin of the textimonail row */}
+        <Box className="about__client">
+          <h2>Our Client's Stories</h2>
+          <Box className="about-container container2">
+            <Flex justifyContent="around">
+              <Image
+                src={aboutUs2}
+                alt="About Us Image"
+                width={550}
+                height={550}
+              />
+              <Box className="about__container__text ">
+                <h3>#HARDFACT</h3>
+                <h1> CHANGING THE FUTURE</h1>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Praesentium, explicabo? Laudantium, sed blanditiis modi optio
+                  cupiditate unde ut necessitatibus, pariatur iste quis
+                  explicabo architecto labore debitis officia ipsam natus
+                  molestias quas ullam saepe? Eveniet culpa delectus
+                  reprehenderit ut vitae atque provident saepe aperiam
+                  praesentium? Natus accusamus, voluptate temporibus magni quo
+                  amet, perferendis commodi aut error doloribus ex at nostrum
+                  deserunt!
+                </p>
+                <h3>Mrs Smith ERICK</h3>
+              </Box>
+            </Flex>
+          </Box>
+        </Box>
       </Box>
     </>
   );
