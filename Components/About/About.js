801cb9dcad22b88dcfcdfadfdfd3c5e6810b31e8
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-02-03 13:08:11 +0100

    imported header to the about page

--- Components/About/About.js
+++ Components/About/About.js
@@ -1,10 +1,23 @@
+import { Box } from "@chakra-ui/react";
+import Head from "next/head";
 import React from "react";
+import Header from "../Header";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <div className="">
+        <Head>
+          <title>About | Florintech Computer College</title>
+          <meta
+            name="courses"
+            content="you can select from our wide range of courses"
+          />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <Header />
+      </div>
+    </>
   );
 };
 
