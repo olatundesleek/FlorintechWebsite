2130829e795ccc57e27fe99b185f8efc50e88260
olatundesleek <olatundesleek@gmail.com>
2023-04-25 14:29:34 +0100

    added logo as default open graph image

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,7 +3,16 @@ export default {
     type: "website",
     locale: "en_IE",
     url: "https://florintechcomputercollege.com/",
-    siteName: "SiteName",
+    siteName: "Florintech Computer College",
+    images: [
+      {
+        url: "https://www.florintechcomputercollege.com/images/logo.png",
+        width: 800,
+        height: 600,
+        alt: "Florintech Computer College Logo",
+        type: "image/png",
+      },
+    ],
   },
   twitter: {
     handle: "@Florintechcc",
