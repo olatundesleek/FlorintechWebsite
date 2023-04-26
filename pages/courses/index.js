a5bc8459c88444a3620fe8719af5b4f50b512fc0
Olatunde Sleek <olatundesleek@gmail.com>
2023-04-26 19:40:49 +0100

    fixed module error

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,7 +1,7 @@
 import { Box, Text, Grid } from "@chakra-ui/react";
 import React from "react";
 import { DefaultSeo } from "next-seo";
-import SEO from "../next-seo.config";
+import SEO from "../../next-seo.config";
 import CourseTab from "../../Components/Coursetab/CourseTab";
 import Header from "../../Components/Header";
 import { courseImages } from "../../Components/images";
